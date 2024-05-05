8a3ebbec82bcdf1dc042a7d965827ff3c2392393
JaxsonOConnor <jaxoco2009@gmail.com>
2024-05-05 17:05:09 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.querySelectorAll("[hover-text]").forEach((element) => {
+//     let hover_text = element.getAttribute("hover-text")
+
+//     element.addEventListener("mouseover", () => {
+//         element.innerHTML += hover_text
+//     })
+//     element.addEventListener("mouseout", () => {
+//         element.innerHTML = element.innerHTML.replace(hover_text, "");
+//     })
+// })
